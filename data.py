739509edd0dc9b09ec63e2f875c98d4e39841702
ANEUDIS MAIN.PY <aneudis1010@gmail.com>
2025-12-20 17:05:28 -0500

    commiting these

---
+++ data.py
@@ -0,0 +1,8 @@
+URBAN_ROUTES_URL = 'https://cnt-0e3c769e-bb5e-4b37-ab21-aa62a916b89d.containerhub.tripleten-services.com/'
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
